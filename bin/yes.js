1569088f129a8abcab79f7c7d1886090fa271c8e
zhufeng <zhufeng@bokesoft.com>
2018-09-18 16:13:34 +0800

    add yes-cli

---
+++ bin/yes.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+// Check node version before requiring/doing anything else
+// The user may be on a very old node version
+const program = require('commander');
+program
+    .version(require('../package').version)
+    .usage('<command> [options]');
+program
+    .command('create <app-name>');
